28010024939306ea509544f2e8039abf467f7149
Alejandro <alejandroidb@gmail.com>
2024-06-26 17:50:12 -0400

    cambios menores

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,9 @@ $(document).ready(function() {
   let listaPelicula = []; // Variable global para almacenar la lista de películas
 
   const obtenerPelicula = async () => {
-    try {
-      const response = await fetch('https://www.codigo-alfa.cl/aglo/tester/listasPeliculas');
-      const peliculasList = await response.json();
+    $.get('https://www.codigo-alfa.cl/aglo/tester/listasPeliculas', function(peliculasList) {
       listaPelicula = peliculasList.peliculas; // Actualiza la variable global con la lista de películas
+      console.log(listaPelicula)
 
       lista.empty(); // Asegura que el <select> está vacío antes de agregar nuevas opciones
 
@@ -18,9 +17,9 @@ $(document).ready(function() {
         const opcion = $('<option></option>').attr('value', pelicula.title).text(pelicula.title);
         lista.append(opcion);
       });
-    } catch (error) {
-      console.error('Hubo un problema con la solicitud Fetch:', error);
-    }
+    }).fail(function(error) {
+      console.error('Hubo un problema con la solicitud GET:', error);
+    });
   };
 
   const mostrarDetalles = () => {
@@ -49,7 +48,7 @@ $(document).ready(function() {
         <p>Año: ${peliculaRandom.year}</p>
       `);
     } else {
-      details.html('<p>No se encontraron detalles para esta película aleatoria.</p>');
+      details2.html('<p>No se encontraron detalles para esta película aleatoria.</p>');
     }
   };
 
